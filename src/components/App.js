e419da056a75e68d2ce7b8710c7500d96cd1c0b6
ta1kt0me <p.wadachi@gmail.com>
2019-08-30 23:43:46 +0900

    Format

--- src/components/App.js
+++ src/components/App.js
@@ -56,15 +56,15 @@ class App extends React.Component {
             appName={this.props.appName}
             currentUser={this.props.currentUser} />
             <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/editor/:slug" component={Editor} />
-            <Route path="/editor" component={Editor} />
-            <Route path="/article/:id" component={Article} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/@:username/favorites" component={ProfileFavorites} />
-            <Route path="/@:username" component={Profile} />
+              <Route exact path="/" component={Home}/>
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/editor/:slug" component={Editor} />
+              <Route path="/editor" component={Editor} />
+              <Route path="/article/:id" component={Article} />
+              <Route path="/settings" component={Settings} />
+              <Route path="/@:username/favorites" component={ProfileFavorites} />
+              <Route path="/@:username" component={Profile} />
             </Switch>
         </div>
       );
